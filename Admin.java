cd980d6a48613bdf6fe071f8dda81dc999aa2dbc
jun_v <159228500+hiraio@users.noreply.github.com>
2024-03-18 07:32:36 +0900

    Update Admin.java

--- Admin.java
+++ Admin.java
@@ -1,42 +1,62 @@
 package edu.java.side;
 
-
+import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-
-import javax.swing.JTextArea;
 import javax.swing.JScrollPane;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import java.awt.Font;
 
 public class Admin extends JFrame {
 
+	private JPanel contentPane;
+	private JTextField textField;
 
 	/**
-	 * 
+	 * Launch the application.
 	 */
-	
-	private static final long serialVersionUID = 1L;
-	private JPanel contentPane;
-
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Admin frame = new Admin();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
 
 	/**
 	 * Create the frame.
 	 */
 	public Admin() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 577, 497);
+		setBounds(100, 100, 527, 478);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JTextArea scrollPane = new JTextArea();
-		scrollPane.setBounds(31, 162, 76, -130);
+		JScrollPane scrollPane = 
+				new JScrollPane();
+		scrollPane.setBounds(29, 57, 454, 195);
 		contentPane.add(scrollPane);
 		
-		JScrollPane scrollPane_1 = new JScrollPane();
-		scrollPane_1.setBounds(31, 29, 507, 166);
-		contentPane.add(scrollPane_1);
+		textField =  new JTextField();
+		scrollPane.setViewportView(textField);
+		
+		textField.setColumns(10);
+	
+		
+		JLabel lblAssignInfo = new JLabel("회원정보");
+		lblAssignInfo.setFont(new Font("굴림", Font.PLAIN, 20));
+		lblAssignInfo.setBounds(192, 262, 170, 76);
+		contentPane.add(lblAssignInfo);
 	}
 }
